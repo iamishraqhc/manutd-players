8f7a4037a07ff6dc597d69a565035a10167a7b78
Ishraq Haider Chowdhury <ishraq-haider.chowdhury@stud.uni-bamberg.de>
2021-08-16 12:02:43 +0200

    updated json and index

--- index.js
+++ index.js
@@ -16,21 +16,21 @@ app.get('/players', (req, res) => {
     res.send(importData)
 })
 
-app.get('/goalkeepers', (req, res) => {
-    res.send(importData.goalkeepers)
-})
+// app.get('/goalkeepers', (req, res) => {
+//     res.send(importData.goalkeepers)
+// })
 
-app.get('/defenders', (req, res) => {
-    res.send(importData.defenders)
-})
+// app.get('/defenders', (req, res) => {
+//     res.send(importData.defenders)
+// })
 
-app.get('/midfielders', (req, res) => {
-    res.send(importData.midfielders)
-})
+// app.get('/midfielders', (req, res) => {
+//     res.send(importData.midfielders)
+// })
 
-app.get('/forwards', (req, res) => {
-    res.send(importData.forwards)
-})
+// app.get('/forwards', (req, res) => {
+//     res.send(importData.forwards)
+// })
 
 app.listen(port, () => {
     console.log(`App is listening to port ${port}`)
